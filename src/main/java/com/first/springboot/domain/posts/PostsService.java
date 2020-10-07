5a623292576bde37535b29ba1dca9c67479b22b6
Sonny HY <hoson1@coupang.com>
2020-10-08 01:26:42 +0900

    전체조회, 수정, 삭제 기능 구현

--- src/main/java/com/first/springboot/domain/posts/PostsService.java
+++ src/main/java/com/first/springboot/domain/posts/PostsService.java
@@ -1,5 +1,6 @@
 package com.first.springboot.domain.posts;
 
+import com.first.springboot.web.dto.PostsListResponseDto;
 import com.first.springboot.web.dto.PostsResponseDto;
 import com.first.springboot.web.dto.PostsSaveRequestDto;
 import com.first.springboot.web.dto.PostsUpdateRequestDto;
@@ -7,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -26,9 +29,22 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Not Existed content. id =" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllByOrderByIdDesc().stream()
+                .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        postsRepository.delete(postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Not Existed content. id =" + id)));
     }
 }
--- src/main/java/com/first/springboot/web/PostsApiController.java
+++ src/main/java/com/first/springboot/web/PostsApiController.java
@@ -4,6 +4,7 @@ import com.first.springboot.domain.posts.PostsService;
 import com.first.springboot.web.dto.PostsResponseDto;
 import com.first.springboot.web.dto.PostsSaveRequestDto;
 import com.first.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,5 +27,11 @@ public class PostsApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }
